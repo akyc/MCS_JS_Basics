657c1c53415872429a991d42f214bd3be7515eca
akyc <23010109+akyc@users.noreply.github.com>
2019-11-12 01:35:59 +0300

    add practika-2
    2 homework

---
+++ practika-2.js
@@ -0,0 +1,7 @@
+let moneyWithYou = prompt('Cколько у вас с собой есть денег?'),
+	applePayed = prompt('Cколько вы купили яблок?'),
+	breadPayed = prompt('Cколько вы купили батонов хлеба?'),
+	appleCost = parseFloat(applePayed) * parseFloat(prompt('Cколько стоит одно яблоко?')),
+	breadCost = parseFloat(breadPayed) * parseFloat(prompt('Cколько стоит один батон хлеба?'));
+
+document.write(moneyWithYou >= appleCost + breadCost);
