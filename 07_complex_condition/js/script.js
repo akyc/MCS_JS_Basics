b4a73bc6a42d76238ece9290c9db69512f769376
akyc <23010109+akyc@users.noreply.github.com>
2019-11-17 15:04:22 +0300

    4.3.1

---
+++ 07_complex_condition/js/script.js
@@ -0,0 +1,10 @@
+console.log("You are at " + window.location);
+
+let night, tired;
+night = false;
+tired = true;
+if(night || tired){
+    console.log('relax');
+}else {
+    console.log('code a little bit');
+}
